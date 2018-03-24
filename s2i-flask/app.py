971a9ac1b03d910e1c6fcdf99832bf0cafe594d9
Martin Eggen <meggen@redhat.com>
2018-03-24 13:32:36 +0100

    Add template for index page, add feature toggle demo

--- s2i-flask/app.py
+++ s2i-flask/app.py
@@ -1,27 +1,37 @@
 import os
-from flask import Flask, jsonify, abort, redirect, url_for
+from flask import Flask, jsonify, abort, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 APP_HEALTH_OK = True
-APP_VERSION=1.0
+APP_VERSION = 1.0
+
 
 @app.route("/")
 def index():
-  return "Hello OCP world from {} running version {}".format(os.environ.get('HOSTNAME', 'localhost'), APP_VERSION)
+    features = [os.environ.get(feat)
+                for feat in os.environ if feat.startswith("FEAT_")]
+
+    return render_template('index.html',
+                           version=APP_VERSION,
+                           name=os.environ.get("HOSTNAME", "localhost"),
+                           features=features)
+
 
 @app.route("/healthz")
 def healthz():
-  if APP_HEALTH_OK:
-    return jsonify(healthy=APP_HEALTH_OK, version=APP_VERSION)
-  else:
-    abort(500)
+    if APP_HEALTH_OK:
+        return jsonify(healthy=APP_HEALTH_OK, version=APP_VERSION)
+    else:
+        abort(500)
+
 
 @app.route("/invalidate")
 def invalidate():
-  global APP_HEALTH_OK
-  APP_HEALTH_OK = False
-  
-  abort(500)
+    global APP_HEALTH_OK
+    APP_HEALTH_OK = False
+
+    abort(500)
+
 
 app.run(host="0.0.0.0", port=8080, debug=True)
