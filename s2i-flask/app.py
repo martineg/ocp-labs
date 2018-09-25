0cbfed6151a5777c3211bacb6b2a5778a5bc14c6
Martin Eggen <meggen@redhat.com>
2018-09-25 17:16:14 +0300

    Simple content negotiation

--- s2i-flask/app.py
+++ s2i-flask/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, abort, redirect, url_for, render_template
+from flask import Flask, request, jsonify, abort, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -7,9 +7,20 @@ APP_HEALTH_OK = True
 APP_VERSION = 1.0
 FEATURES = [ (feat, os.environ.get(feat)) for feat in os.environ if feat.startswith("FEAT_")]
 
+# http://flask.pocoo.org/snippets/45/
+def request_wants_json():
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+        request.accept_mimetypes[best] > \
+        request.accept_mimetypes['text/html']
 
 @app.route("/")
 def index():
+    if request_wants_json():
+        return jsonify(message="Hello OCP world!", version=APP_VERSION,
+                    hostname=os.environ.get("HOSTNAME", "localhost"),
+                    features=dict(FEATURES))
     return render_template('index.html',
                            version=APP_VERSION,
                            name=os.environ.get("HOSTNAME", "localhost"),
