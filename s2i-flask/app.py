c79dfe449f0e10f0303f7baed1daf87a52770f58
Martin Eggen <meggen@redhat.com>
2020-09-10 16:55:07 +0200

    Add fibonacci function
    Add route to calculate fibonacci numbers for simple load generation/testing

--- s2i-flask/app.py
+++ s2i-flask/app.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 from flask import Flask, request, jsonify, abort, redirect, url_for, render_template
 
 app = Flask(__name__)
@@ -6,6 +6,11 @@ app = Flask(__name__)
 APP_HEALTH_OK = True
 APP_VERSION = 1.0
 FEATURES = [ (feat, os.environ.get(feat)) for feat in os.environ if feat.startswith("FEAT_")]
+
+def fib(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fib(n-1) + fib(n-2)
 
 @app.route("/")
 def index():
@@ -39,6 +44,13 @@ def invalidate():
     APP_HEALTH_OK = False
 
     abort(500)
+
+@app.route("/fibonacci/<int:fib_num>")
+def fibonacci(fib_num):
+    request_start_time = time.time()
+    res = fib(fib_num)
+    return jsonify(fibonacci=res,
+                   elapsed_time=time.time() - request_start_time)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
