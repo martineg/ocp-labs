b4202d31444fee3c0d27a52d25ac3bcb7aa9a85f
Martin Eggen <meggen@redhat.com>
2024-05-31 16:07:05 +0200

    No need to print result of health probes

--- s2i-flask/app.py
+++ s2i-flask/app.py
@@ -21,10 +21,11 @@ def healthy():
     else:
         return "ok"
 def liveness():
-    print(healthy())
+    healthy()
 
 def readiness():
-    print(healthy())
+    healthy()
+
 app.config.update(
     HEALTHZ = {
         "live" : "app.liveness",
