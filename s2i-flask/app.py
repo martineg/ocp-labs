6f79f09bda158c785a63c4706663dd08b0428e9b
Martin Eggen <meggen@redhat.com>
2024-05-31 12:42:46 +0200

    Implement live and readiness endpoints with flask-healthz (#8)

--- s2i-flask/app.py
+++ s2i-flask/app.py
@@ -1,7 +1,10 @@
 import os, time
 from flask import Flask, request, jsonify, abort, redirect, url_for, render_template
+from flask_healthz import healthz, HealthError
+
 
 app = Flask(__name__)
+app.register_blueprint(healthz, url_prefix="/healthz")
 
 APP_HEALTH_OK = True
 APP_VERSION = 1.0
@@ -11,6 +14,23 @@ def fib(n):
     if n == 0: return 0
     elif n == 1: return 1
     else: return fib(n-1) + fib(n-2)
+
+def healthy():
+    if not APP_HEALTH_OK:
+        raise HealthError("error")
+    else:
+        return "ok"
+def liveness():
+    print(healthy())
+
+def readiness():
+    print(healthy())
+app.config.update(
+    HEALTHZ = {
+        "live" : "app.liveness",
+        "ready" : "app.readiness",
+    }
+)
 
 @app.route("/")
 def index():
@@ -28,15 +48,6 @@ def index():
 def version():
     return jsonify(version=APP_VERSION, features=FEATURES,
         hostname=os.environ.get("HOSTNAME", "localhost"))
-
-
-@app.route("/healthz")
-def healthz():
-    if APP_HEALTH_OK:
-        return jsonify(healthy=APP_HEALTH_OK)
-    else:
-        abort(500)
-
 
 @app.route("/invalidate")
 def invalidate():
